efcfc450f62f08f72533c4f49ebabe36321bc973
ThomasDescroix <descroix.thomas@gmail.com>
2022-02-15 22:43:03 +0100

    ✨  add until destroy to unsubscribe from observables

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { UntilDestroy } from '@ngneat/until-destroy';
+import { finalize, fromEvent, Subscription } from 'rxjs';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
+@UntilDestroy({ checkProperties: true })
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -9,11 +12,14 @@ import { HeroService } from '../hero.service';
 })
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
+  private mousemoveSubscription!: Subscription;
 
   constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
+    this.mousemoveSubscription = fromEvent(document, 'mousemove')
+      .subscribe(() => console.log('mousemove'));
   }
 
   getHeroes(): void {
